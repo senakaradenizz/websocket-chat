269c3d5ae0e65dc50b69459e9e0a864c5bf1908e
senakaradeniz <sena.karadeniz@ogr.sakarya.edu.tr>
2020-05-10 16:28:54 +0300

    added project files

---
+++ ChatApplications/ChatApplications/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ChatApplications.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace ChatApplications.Controllers
+{
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View("InsertUserName");
+		}
+
+		[HttpPost]
+		public IActionResult Index(string username)
+		{
+			return View("Index", username);
+		}
+	}
+}
---
+++ ChatApplications/ChatApplications/SaveMessages.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using ChatApplications.Models;
+using System.Text;
+
+namespace ChatApplications
+{
+	public class SaveMessages
+	{
+		public void SaveMessage(string message) {
+
+
+			string fileName = @"C:\Messages.txt";
+			string textToAdd = message;
+			FileStream fs = null;
+			try
+			{
+				fs = new FileStream(fileName,FileMode.Append);
+				using (StreamWriter writer = new StreamWriter(fs))
+				{
+						writer.WriteLine(textToAdd+"|");
+				}
+			}
+			finally
+			{
+				if (fs != null)
+					fs.Dispose();
+			}
+
+			
+		}
+
+		public string ChatHistory()
+		{
+			string fileName = @"C:\Messages.txt";
+			string s = "";
+
+			using (StreamReader sr = File.OpenText(fileName))
+			{
+				StringBuilder stringBuilder = new StringBuilder();
+				
+
+				while ((s = sr.ReadLine()) != null)
+				{
+					stringBuilder.AppendLine(s);
+					
+				}
+				 
+				s = stringBuilder.ToString();
+			}
+			
+			return s;
+		}
+	}
+}
